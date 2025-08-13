71e77805eaae45b8286162bca76c17e8d6d1ba37
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-08-13 16:47:02 +0600

    Learn : React-toolkit

---
+++ redux-toolkit-ptactice/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Task from "./components/Task";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div>
+      <h3>Hello world</h3>
+      <Task/>
+      <TaskList/>
+    </div>
+    
+  );
+}
+
+export default App;
