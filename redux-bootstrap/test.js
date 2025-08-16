b880184bd5c3e8c88a18fb43821c1e1f2a46b9ad
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-08-16 16:17:50 +0600

    Learn : pure, impure function

---
+++ redux-bootstrap/test.js
@@ -0,0 +1,20 @@
+
+
+// pure function
+
+const add = (a , b ) => a + b;
+// console.log(add(5, 10)); // 15
+// console.log(add(5, 10)); // 15
+// console.log(add(5, 10)); // 15
+
+// impure function
+
+const randomNumber = (amount) => {
+  return amount + Math.random();
+};
+
+// console.log(randomNumber(3));
+// console.log(randomNumber(3));
+// console.log(randomNumber(3));
+
+// Functional programming always prefer pure function
