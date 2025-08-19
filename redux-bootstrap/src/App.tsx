7c3af916c61359423bde58b0eae65c4920ae65dc
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-08-19 16:38:28 +0600

    Learn : Install ShadCN and use them into project

--- redux-bootstrap/src/App.tsx
+++ redux-bootstrap/src/App.tsx
@@ -1,40 +1,71 @@
-import './App.css';
-import { decrement, increment, incrementByFive } from './redux/features/counter/counterSlice';
-import { useAppDispatch, useAppSelector } from './redux/hooks';
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
 
 function App() {
-
-  const dispatch = useAppDispatch();
-  const counter = useAppSelector((state) => state);
-
-
-  // handle increment
-  const handleIncrement = () => {
-    dispatch(increment())
-  }
-
-  // handle increment by 5 and with payload
-  const handleIncrementByFive = (amount:number) => {
-    dispatch(incrementByFive(amount))
-  }
-
-
-
-  // handle decrement
-  const handleDecrement = () => {
-    dispatch(decrement())
-  }
-
   return (
-    <>
-    <div>
-      <h3>Count : {counter.count}</h3>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={() => handleIncrementByFive(5)}>Increment by 5</button>
-      <button onClick={handleDecrement}>Decrement</button>
+    <div className="w-5/6 mx-auto">
+      <h2>Basic todo Application</h2>
+      <div className="my-4">
+        <Button className="cursor-pointer ">Click me</Button>
+      </div>
+      {/* card */}
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">Sign Up</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
