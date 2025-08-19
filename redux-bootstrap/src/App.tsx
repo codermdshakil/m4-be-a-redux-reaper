b31a1bca1f125ca3253ad6c00c0db51fed2cb073
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-08-20 01:16:47 +0600

    Learn : Dark and Light Mode

--- redux-bootstrap/src/App.tsx
+++ redux-bootstrap/src/App.tsx
@@ -3,12 +3,14 @@ import Navber from "./components/layout/Navber";
 
 function App() {
   return (
-    <div>
-      <Navber />
-      <div className="w-5/6 mx-auto py-4">
-        <Outlet />
+    
+      <div>
+        <Navber />
+        <div className="w-5/6 mx-auto py-4">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    
   );
 }
 
--- redux-bootstrap/src/components/layout/Navber.tsx
+++ redux-bootstrap/src/components/layout/Navber.tsx
@@ -1,4 +1,13 @@
+import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router";
+import { useTheme } from "../../providers/theme-provider";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,24 +16,61 @@ import {
 } from "../ui/navigation-menu";
 
 const Navber = () => {
+  const { setTheme } = useTheme();
+
   return (
-    <div className="bg-gray-300 p-4 flex justify-center" > 
-      <NavigationMenu viewport={false}>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="cursor-pointer"><Link to={"/"}>Home</Link></NavigationMenuTrigger>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="cursor-pointer"><Link to={"/users"}>Users</Link></NavigationMenuTrigger>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="cursor-pointer"><Link to={"/tasks"}>Tasks</Link></NavigationMenuTrigger>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="cursor-pointer"><Link to={"/about"}>About</Link></NavigationMenuTrigger>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className="bg-gray-300">
+      <div className="w-5/6 mx-auto py-4 flex justify-between">
+        <div>
+          <NavigationMenu viewport={false}>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="cursor-pointer">
+                  <Link to={"/"}>Home</Link>
+                </NavigationMenuTrigger>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="cursor-pointer">
+                  <Link to={"/users"}>Users</Link>
+                </NavigationMenuTrigger>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="cursor-pointer">
+                  <Link to={"/tasks"}>Tasks</Link>
+                </NavigationMenuTrigger>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="cursor-pointer">
+                  <Link to={"/about"}>About</Link>
+                </NavigationMenuTrigger>
+              </NavigationMenuItem>
+              <NavigationMenuItem></NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
     </div>
   );
 };
